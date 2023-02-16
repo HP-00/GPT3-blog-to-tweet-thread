e95f1cbd22a676b7db32c182109c9fa31f23e0ff
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-02-16 19:22:40 +0000

    Added API endpoint for OpenAI, Generate button linked to API, GPT-3 output added to UI, loading state added to generate button, prompt added, temperature and max tokens set

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+I am going to provide you with a blog post I have written, I want you to convert it into a tweet thread, number the tweets and show breaks in between, make sure the whole tweet thread is able to include the important points of my blog post, the tweets need to be concise and include relevant emojis.
+
+Blog post:
+`;
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 1000,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -16,19 +38,46 @@ const Home = () => {
             <h1>blog post to tweet thread</h1>
           </div>
           <div className="header-subtitle">
-            <h2>paste your blog post below, we'll transform it into a tweet thread</h2>
+            <h2>
+              paste your blog post below, we'll transform it into a tweet thread
+            </h2>
           </div>
         </div>
         <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" value={userInput}
-  onChange={onUserChangedText}/>
-  <div className="prompt-buttons">
-    <a className="generate-button" onClick={null}>
-      <div className="generate">
-        <p>Generate</p>
-      </div>
-    </a>
-  </div>
+          <textarea
+            placeholder="input your blog post here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
